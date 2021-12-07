29dd717c94c96a825368ba646e630b09ad0111eb
Wassim Arfa <wassim.arfa@gmail.com>
2021-12-07 12:50:47 +0100

    feat: home story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
@@ -7,5 +9,14 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ]
+    return config
   },
 }
