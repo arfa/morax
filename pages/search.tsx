f2c19fc35709ca4a8156640eaf84c81990d5d872
Wassim Arfa <wassim.arfa@gmail.com>
2021-12-13 11:28:49 +0100

    fix: sonar issues

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,23 +1,9 @@
 import Filters from '@components/filter/Filters'
 import ProductCard from '@components/product/product-card'
-import { Product } from '@components/types'
 import { Container, Grid, Paper, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import * as React from 'react'
-
-const products: Product[] = [
-  {
-    id: '0',
-    image:
-      'https://cdn.shopify.com/s/files/1/0752/6435/products/Shade2.jpg?v=1607128206',
-    price: 25,
-    sale_price: 20,
-    quantity: 10,
-    name: 'T-shirt',
-    description:
-      'Incididunt minim anim fugiat consectetur elit consectetur culpa...',
-  },
-]
+import { products } from '../public/data-mock'
 
 const Home: NextPage = () => {
   return (
