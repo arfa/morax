b386130a185d98479bc75c70da4c1ad2acfa0fc0
Wassim Arfa <wassim.arfa@gmail.com>
2021-12-16 18:06:27 +0100

    feat: search

--- components/category/category-list.tsx
+++ components/category/category-list.tsx
@@ -5,6 +5,8 @@ import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import * as React from 'react'
 import { category } from '../types'
+import Link from 'next/link'
+import { getCategoryPath } from '@lib/search'
 interface Props {
   data: category[]
 }
@@ -25,24 +27,27 @@ export default function CategoryList({ data }: Props) {
           Categories
         </ListSubheader>
         {data.map((category) => (
-          <Card
+          <Link
             key={category.id}
-            sx={{ marginY: 1, bgcolor: 'background.paper' }}
+            href={{ pathname: getCategoryPath(category.path) }}
+            passHref
           >
-            <ListItemButton
-              selected={selectedIndex.toString() === category.id}
-              onClick={(event) => handleListItemClick(event, category.id)}
-            >
-              <ListItemAvatar>
-                <Avatar
-                  alt={category.name}
-                  src={category.image?.url}
-                  sx={{ width: 56, height: 56, marginRight: 2 }}
-                />
-              </ListItemAvatar>
-              <ListItemText primary={category.name} />
-            </ListItemButton>
-          </Card>
+            <Card sx={{ marginY: 1, bgcolor: 'background.paper' }}>
+              <ListItemButton
+                selected={selectedIndex.toString() === category.id}
+                onClick={(event) => handleListItemClick(event, category.id)}
+              >
+                <ListItemAvatar>
+                  <Avatar
+                    alt={category.name}
+                    src={category.image?.url}
+                    sx={{ width: 56, height: 56, marginRight: 2 }}
+                  />
+                </ListItemAvatar>
+                <ListItemText primary={category.name} />
+              </ListItemButton>
+            </Card>
+          </Link>
         ))}
       </List>
     </Box>
