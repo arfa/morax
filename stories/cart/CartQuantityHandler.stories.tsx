dcd742097279f029817be2403ced061a650d9043
Wassim Arfa <wassim.arfa@gmail.com>
2021-12-20 15:35:51 +0100

    feat: cart-product-subtitle

--- stories/cart/CartQuantityHandler.stories.tsx
+++ stories/cart/CartQuantityHandler.stories.tsx
@@ -4,14 +4,14 @@ import { useState } from 'react'
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/CartQuantityHandler',
+  title: 'cart/CartQuantityHandler',
   component: CartQuantityHandler,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
 } as ComponentMeta<typeof CartQuantityHandler>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof CartQuantityHandler> = (args) => {
+const Template: ComponentStory<typeof CartQuantityHandler> = () => {
   const [quantity, setQuantity] = useState<number>(1)
   return (
     <CartQuantityHandler
