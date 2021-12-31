54375dc59063ed15bb115ca404e0998711a4b3d7
Wassim Arfa <wassim.arfa@gmail.com>
2021-12-31 17:02:03 +0100

    fix: wishlist

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -1,3 +1,4 @@
+import ProductCardLoader from '@components/loaders/product-card-loader'
 import useWishlist from '@framework/wishlist/use-wishlist'
 import { Container, Grid, Typography } from '@mui/material'
 import type { NextPage } from 'next'
@@ -6,40 +7,43 @@ import ProductCardContainer from '../containers/product/ProductCardContainer'
 const Wishlist: NextPage = () => {
   // @ts-ignore Shopify - Fix this types
   const { data, isLoading, isEmpty } = useWishlist({ includeProducts: true })
+  console.log(data, isLoading, isEmpty)
+  let wishlistContent = null
+  if (isEmpty && data?.items.length == 0) {
+    wishlistContent = <p>Your wishlist is empty</p>
+  }
 
-  return (
-    <>
-      <Container maxWidth="xl" sx={{ marginTop: '50px' }}>
-        {/* Related Products */}
-
-        <Typography variant="h5" paddingBottom="40px">
-          My Wishlist
-        </Typography>
-        {isLoading ? (
-          <p>loading...</p>
-        ) : isEmpty ? (
-          <p>Your wishlist is empty</p>
-        ) : (
-          <Grid container spacing={2} border={0}>
-            {data &&
-              // @ts-ignore Shopify - Fix this types
-              data.items?.map((item) => (
-                <Grid
-                  key={item.id}
-                  item
-                  xs={12}
-                  sm={6}
-                  md={2}
-                  padding={0}
-                  border={0}
-                >
-                  <ProductCardContainer product={item.product! as any} />
-                </Grid>
-              ))}
+  if (isLoading) {
+    wishlistContent = (
+      <Grid container spacing={2} border={0}>
+        {[1, 2, 3, 4]?.map((item) => (
+          <Grid key={item} item xs={12} sm={6} md={2} padding={0} border={0}>
+            <ProductCardLoader />
           </Grid>
-        )}
-      </Container>
-    </>
+        ))}
+      </Grid>
+    )
+  }
+
+  if (!isLoading && !isEmpty) {
+    wishlistContent = (
+      <Grid container spacing={2} border={0}>
+        {data.items?.map((item: any) => (
+          <Grid key={item.id} item xs={12} sm={6} md={2} padding={0} border={0}>
+            <ProductCardContainer product={item.product! as any} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  }
+  return (
+    <Container maxWidth="xl" sx={{ marginTop: '50px' }}>
+      {/* Related Products */}
+      <Typography variant="h5" paddingBottom="40px">
+        My Wishlist
+      </Typography>
+      {wishlistContent}
+    </Container>
   )
 }
 
