e5b1995224747cfdabbcfd4f7632d57d41e38341
Marwen Arfa <marwen.arfa@gmail.com>
2022-01-01 08:37:10 +0100

    refactor(team-thor): wishlist page

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -4,22 +4,19 @@ import { Container, Grid, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import ProductCardContainer from '../containers/product/ProductCardContainer'
 
-const Wishlist: NextPage = () => {
-  // @ts-ignore Shopify - Fix this types
+const WishlistContent = () => {
   const { data, isLoading, isEmpty } = useWishlist({ includeProducts: true })
-  console.log('isLoading: ', isLoading);
 
-  let wishlistContent = null
   if (isEmpty && data?.items.length == 0) {
-    wishlistContent = <p>Your wishlist is empty</p>
+    return <p>Your wishlist is empty</p>
   }
 
   if (isLoading) {
-    wishlistContent = <ProductListLoader number={3} />
+    return <ProductListLoader number={3} />
   }
 
   if (!isLoading && !isEmpty) {
-    wishlistContent = (
+    return (
       <Grid container spacing={2} border={0}>
         {data.items?.map((item: any) => (
           <Grid key={item.id} item xs={12} sm={6} md={2} padding={0} border={0}>
@@ -29,13 +26,20 @@ const Wishlist: NextPage = () => {
       </Grid>
     )
   }
+
+  return null;
+
+}
+
+const Wishlist: NextPage = () => {
+  // @ts-ignore Shopify - Fix this types
   return (
     <Container maxWidth="xl" sx={{ marginTop: '50px' }}>
       {/* Related Products */}
       <Typography variant="h5" paddingBottom="40px">
         My Wishlist
       </Typography>
-      {wishlistContent}
+      <WishlistContent />
     </Container>
   )
 }
