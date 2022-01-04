2b9a3ca064b9d8909307d6fb2046ef7a0f88921a
Wassim Arfa <wassim.arfa@gmail.com>
2022-01-04 20:36:22 +0100

    feat: back-top story

---
+++ components/layouts/scroll-top.stories.tsx
@@ -0,0 +1,28 @@
+import ScrollTop from '@components/layouts/scroll-top'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+import { Box } from '@mui/material'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Layouts/ScrollTop',
+  component: ScrollTop,
+
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof ScrollTop>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ScrollTop> = (args) => (
+  <>
+    <Box id="top" sx={{ height: '100vh' }}></Box>{' '}
+    <ScrollTop
+      {...args}
+      sx={{ transform: 'scale(1)', visibility: 'visible' }}
+    />
+  </>
+)
+
+export const Default = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = { anchorTop: '#top' }
