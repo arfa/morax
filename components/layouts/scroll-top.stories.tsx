e8833a4634d05c9c98b3590ec2abc072fe854cd2
Marwen Arfa <marwen.arfa@gmail.com>
2022-01-06 01:43:40 +0100

    fix(component): fix scroll top

--- components/layouts/scroll-top.stories.tsx
+++ components/layouts/scroll-top.stories.tsx
@@ -14,27 +14,57 @@ export default {
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof ScrollTop> = (args) => {
-  const wrapperEl = useRef();
-  const [target, setTarget] = useState();
+  const wrapperEl = useRef()
+  const [target, setTarget] = useState()
 
   useEffect(() => {
-    setTarget(wrapperEl.current);
-  }, [wrapperEl]);
-
-  console.log('wrapperEl: ', wrapperEl);
+    setTarget(wrapperEl.current)
+  }, [wrapperEl])
 
   return (
-  <>
-    <Box
-      ref={wrapperEl}
-      sx={{
-        width: 300,
-        height: 500,
-        overflow: 'scroll',
-        padding: '24px',
-        backgroundColor: 'secondary.light',
-      }}
-    >
+    <>
+      <Box
+        ref={wrapperEl}
+        sx={{
+          width: 300,
+          height: 500,
+          overflow: 'scroll',
+          padding: '24px',
+          backgroundColor: 'secondary.light',
+        }}
+      >
+        {[...Array(5)].map((_, i) => (
+          <Box
+            key={i}
+            sx={{
+              height: 100,
+              margin: 5,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: 'primary.light',
+            }}
+          >
+            scroll down ↓
+          </Box>
+        ))}
+        <ScrollTop target={target} {...args} />
+      </Box>
+    </>
+  )
+}
+
+export const Local = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Local.args = {
+  bottom: 16,
+  right: 16,
+  left: NaN,
+}
+
+const Template2: ComponentStory<typeof ScrollTop> = (args) => {
+  return (
+    <>
       {[...Array(5)].map((_, i) => (
         <Box
           key={i}
@@ -50,15 +80,9 @@ const Template: ComponentStory<typeof ScrollTop> = (args) => {
           scroll down ↓
         </Box>
       ))}
-      <ScrollTop target={target} {...args} />
-    </Box>
-  </>
-)}
-
-export const Default = Template.bind({})
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Default.args = {
-  bottom: 16,
-  right: 16,
-  left: NaN,
+      <ScrollTop {...args} />
+    </>
+  )
 }
+export const Global = Template2.bind({})
+Global.args = {}
