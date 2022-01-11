6df443c890f71016cdcba5e212cb5ed6d5aebcc8
Wassim Arfa <wassim.arfa@gmail.com>
2022-01-11 11:57:30 +0100

    fix: category-list
    fix: category-list

--- components/category/category-list.tsx
+++ components/category/category-list.tsx
@@ -1,9 +1,8 @@
 import { getCategoryPath } from '@lib/search'
-import { Avatar, Card, ListItemAvatar, Typography } from '@mui/material'
+import { Avatar, ListItemAvatar, Typography } from '@mui/material'
 import List from '@mui/material/List'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
-import useMediaQuery from '@mui/material/useMediaQuery'
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
 import Link from 'next/link'
@@ -19,14 +18,23 @@ export default function CategoryList({ data }: Props) {
       '(min-width: 400px)': {
         // tslint:disable-next-line
         slides: { perView: 2, spacing: 5 },
+        loop: true,
       },
-      '(min-width: 1000px)': {
+      '(min-width: 600px)': {
         // tslint:disable-next-line
-        slides: { perView: 3, spacing: 10 },
+        slides: { perView: 3, spacing: 5 },
+        loop: true,
+      },
+      '(min-width: 900px)': {
+        // tslint:disable-next-line
+        slides: { perView: 7, spacing: 5 },
+        vertical: true,
+        loop: false,
       },
     },
     // tslint:disable-next-line
-    slides: { perView: 1 },
+    slides: { perView: 1, spacing: 5 },
+    loop: true,
   })
 
   const handleListItemClick = (
@@ -35,16 +43,12 @@ export default function CategoryList({ data }: Props) {
   ) => {
     setSelectedIndex(Number(index))
   }
-  const downBreakpoint = useMediaQuery((theme: any) =>
-    theme.breakpoints.down('md')
-  )
-  return downBreakpoint ? (
+  return (
     <List
       ref={sliderRef}
       className={'keen-slider'}
       component="nav"
       aria-label="categories"
-      sx={{ display: 'flex', flexDirection: 'row' }}
     >
       {data.map((category: category) => (
         <Link
@@ -52,74 +56,31 @@ export default function CategoryList({ data }: Props) {
           href={{ pathname: getCategoryPath(category.path) }}
           passHref
         >
-          <Card
+          <ListItemButton
             className="keen-slider__slide"
+            selected={selectedIndex.toString() === category.id}
+            onClick={(event) => handleListItemClick(event, category.id)}
             sx={{
               marginBottom: 2,
-              height: 90,
-              boxShadow: 'none',
+              maxHeight: '80px',
               bgcolor: '#f9f9f9',
-              '&:hover': { boxShadow: 'none', bgcolor: '#f1f1f1' },
+              borderRadius: 1,
+              '&:hover': { bgcolor: '#f1f1f1' },
             }}
           >
-            <ListItemButton
-              selected={selectedIndex.toString() === category.id}
-              onClick={(event) => handleListItemClick(event, category.id)}
-              sx={{ transform: 'none' }}
-            >
-              <ListItemAvatar>
-                <Avatar
-                  alt={category.name}
-                  src={category.image?.url}
-                  sx={{ width: 56, height: 56, marginRight: 2 }}
-                />
-              </ListItemAvatar>
-              <ListItemText>
-                <Typography variant="body2" noWrap>
-                  {category.name}
-                </Typography>
-              </ListItemText>
-            </ListItemButton>
-          </Card>
-        </Link>
-      ))}
-    </List>
-  ) : (
-    <List component="nav" aria-label="categories">
-      {data.map((category: category) => (
-        <Link
-          key={category.id}
-          href={{ pathname: getCategoryPath(category.path) }}
-          passHref
-        >
-          <Card
-            sx={{
-              marginBottom: 2,
-              height: 90,
-              boxShadow: 'none',
-              bgcolor: '#f9f9f9',
-              '&:hover': { boxShadow: 'none', bgcolor: '#f1f1f1' },
-            }}
-          >
-            <ListItemButton
-              selected={selectedIndex.toString() === category.id}
-              onClick={(event) => handleListItemClick(event, category.id)}
-              sx={{ transform: 'none' }}
-            >
-              <ListItemAvatar>
-                <Avatar
-                  alt={category.name}
-                  src={category.image?.url}
-                  sx={{ width: 56, height: 56, marginRight: 2 }}
-                />
-              </ListItemAvatar>
-              <ListItemText>
-                <Typography variant="body2" noWrap>
-                  {category.name}
-                </Typography>
-              </ListItemText>
-            </ListItemButton>
-          </Card>
+            <ListItemAvatar>
+              <Avatar
+                alt={category.name}
+                src={category.image?.url}
+                sx={{ width: 56, height: 56, marginRight: 2 }}
+              />
+            </ListItemAvatar>
+            <ListItemText>
+              <Typography variant="body2" noWrap>
+                {category.name}
+              </Typography>
+            </ListItemText>
+          </ListItemButton>
         </Link>
       ))}
     </List>
