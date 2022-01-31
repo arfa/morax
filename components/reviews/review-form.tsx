e4a74b5f22f20bc241405653fb6c5efba3c80fff
Wassim Arfa <wassim.arfa@gmail.com>
2022-01-31 17:25:40 +0100

    Update review-form.tsx

--- components/reviews/review-form.tsx
+++ components/reviews/review-form.tsx
@@ -1,7 +1,6 @@
 import { Card, Rating, Typography } from '@mui/material'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import CssBaseline from '@mui/material/CssBaseline'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
