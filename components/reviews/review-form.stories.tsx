532147416407d4a5581baeb081e4a3d041301609
Wassim Arfa <wassim.arfa@gmail.com>
2022-01-31 17:27:37 +0100

    feat: add-review

---
+++ components/reviews/review-form.stories.tsx
@@ -0,0 +1,26 @@
+import ReviewForm from '@components/reviews/review-form'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Product/ReviewForm',
+  component: ReviewForm,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    onSubmit: { action: 'submitted' },
+  },
+} as ComponentMeta<typeof ReviewForm>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ReviewForm> = (args) => (
+  <ReviewForm {...args} />
+)
+
+export const Default = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  image: 'https://source.unsplash.com/random',
+  title: 'Product Name',
+  rating: 3,
+}
