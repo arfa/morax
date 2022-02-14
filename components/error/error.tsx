a4146394f08f5a702d7c767cae7d993b38806912
Wassim Arfa <wassim.arfa@gmail.com>
2022-02-15 00:34:47 +0100

    feat: simple-fast

--- components/error/error.tsx
+++ components/error/error.tsx
@@ -1,9 +1,19 @@
 import { Button, Stack, Typography } from '@mui/material'
-import Image from 'next/image'
-import { MdHome } from 'react-icons/md'
 import Link from 'next/link'
+import { MdHome } from 'react-icons/md'
 interface Props {
   errorCode?: 404 | 500
+}
+
+const errorTitle = (code: any) => {
+  switch (code) {
+    case 404:
+      return 'Looks like you are lost'
+    case 500:
+      return 'Server Side Error Occured'
+    default:
+      return 'Looks like you are lost'
+  }
 }
 
 export default function Error({ errorCode = 404 }: Props) {
@@ -14,32 +24,17 @@ export default function Error({ errorCode = 404 }: Props) {
       alignItems="center"
       spacing={2}
     >
-      <Image
-        src={`/assets/images/${errorCode}.svg`}
-        alt={`Error ${errorCode}`}
-        width={822}
-        height={492}
-      />
+      <Typography variant="h1" gutterBottom component="div">
+        {errorCode}
+      </Typography>
       <Typography variant="h4" gutterBottom component="div">
-        Looks like you are lost
+        {errorTitle(errorCode)}
       </Typography>
       <Typography variant="subtitle1" gutterBottom component="div">
         We can't find the page you're looking for
       </Typography>
       <Link href="/" passHref>
-        <Button
-          startIcon={<MdHome />}
-          sx={{
-            mt: 3,
-            px: 2,
-            backgroundColor: '#000000',
-            color: '#ffffff',
-            border: 'none',
-            '&:hover': { backgroundColor: '#3a3a3a', border: 'none' },
-          }}
-        >
-          Go Home
-        </Button>
+        <Button startIcon={<MdHome />}>Go Home</Button>
       </Link>
     </Stack>
   )
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,20 +1,5 @@
-import { Stack, Typography } from '@mui/material'
+import Error from '@components/error/error'
 
 export default function Custom500() {
-  return (
-    <Stack
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
-    >
-      <Typography
-        component="h1"
-        variant="h5"
-        sx={{ display: 'flex', alignItems: 'center', height: '70vh' }}
-      >
-        500 - Server-side error occurred
-      </Typography>
-    </Stack>
-  )
+  return <Error errorCode={500} />
 }
